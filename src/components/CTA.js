a8b7a78990b9aa7afbd43403d88b1629875abdaa
rezasetiabudi <shinichisetiabudi@gmail.com>
2021-10-20 21:58:52 +0700

    fix button CTA Whatsapp

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -16,48 +16,22 @@ export const CTA = () => {
   return (
     <Flex
       flexDir={{base:'column', xmstsm:'row'}}
-      // bg='rebeccapurple'
       w='100%'
       flexWrap='wrap'
     >
-      {/* <Flex py='2' pr={{base:0,xsmtsm:2}} w={{base:"100%", xsmtsm:"50%"}}>
-        <Link href="mailto:hellowsuff@gmail.com">
-          <Button 
-            rightIcon={<EmailIcon />} 
-            color="red.500"
-            variant="solid"
-            width="full"
-          >
-            Email
-          </Button>
-        </Link>
-      </Flex> */}
       <Flex py='2' pl={{base:0,xsmtsm:2}} w={{base:"100%", xsmtsm:"50%"}} onClick={e=>window.fbq('track', 'Lead')}>
-        <Link href="https://wa.me/6281214269124?text=Halo%20Wsuff%20Studio">
+        <Link href="https://wa.me/6281214269124?text=Hello%20wsuff...">
           <Button 
             rightIcon={<ChatIcon />} 
             colorScheme="whatsapp" 
             variant="solid"
             width="full"
-            
-            // onClick={{e => window.fbq('track', 'Lead')}}
+            h="40px"
           >
             Whatsapp
           </Button>
         </Link>
       </Flex>
-      {/* <Flex py='2' w="full">
-        <Link href='/blog'>
-          <Button
-            colorScheme="orange"
-            _hover={{ bg: '#f7941e' }}
-            rightIcon={<ChevronRightIcon />} 
-            width="100%"
-            >  
-            Our Portofolio
-          </Button>
-        </Link>
-      </Flex> */}
     </Flex>
   )
 }
